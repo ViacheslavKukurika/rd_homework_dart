805780ce1b55d7797bb4a06ff06039525db8abe9
Viacheslav <viacheslavkukurika@gmail.com>
2026-03-10 11:56:20 +0200

    Initialize Dart project and add first note

---
+++ bin/rd_homework_dart.dart
@@ -0,0 +1,5 @@
+import 'package:rd_homework_dart/rd_homework_dart.dart' as rd_homework_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rd_homework_dart.calculate()}!');
+}
---
+++ test/rd_homework_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:rd_homework_dart/rd_homework_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
